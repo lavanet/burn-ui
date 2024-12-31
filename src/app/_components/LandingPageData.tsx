98809190f3ecdb0454e436a382ff21e82692cf39
OneEuroQuestion <mike@oneeuroquestion.com>
2024-12-31 11:53:48 +0100

    moving things to @burn to src

--- src/app/_components/LandingPageData.tsx
+++ src/app/_components/LandingPageData.tsx
@@ -7,13 +7,13 @@ import { TotalSupplyPieChart } from "./TotalSupplyPieChart";
 export default function LandingPageData() {
   return (
     <>
-      <CardHeader className="flex flex-col items-center justify-center space-y-0 border-b py-7">
-        <CardTitle className="text-3xl font-bold text-center">
+      <CardHeader className="flex flex-col items-center justify-center border-b py-3">
+        <CardTitle className="text-3xl font-bold text-center mt-0 mb-3">
           LAVA Token Burn Statistics
         </CardTitle>
       </CardHeader>
 
-      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3 m-1">
+      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3 m-1 mt-0">
         <DataBox title="Burn % of total supply" value="1.49%" largeValueText={true} />
         <DataBox title="Annualized Burn Percentage" value="1.49%" />
         <DataBox title="LAVA Burned" value="14,911,406.3" />
