903a3e52228cd728c0c0ae909f712bcfbdbcc07a
OneEuroQuestion <mike@oneeuroquestion.com>
2025-01-01 16:32:14 +0100

    3 columns

--- src/app/_components/LandingPageData.tsx
+++ src/app/_components/LandingPageData.tsx
@@ -40,7 +40,7 @@ export default function LandingPageData() {
         <div style={{ marginTop: '10px' }}></div>
         {/* Rest of the boxes in grid */}
         <Card style={{ margin: '15px' }}>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 gap-y-0">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 gap-y-0">
             <DataBox title="Already distributed" value="$413,416" tooltip="Distributed rewards to LAVA stackers in USD" />
 
             {/* <DataBox
