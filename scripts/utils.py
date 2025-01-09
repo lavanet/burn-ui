32578aefb3078dba66585e715ecfd35ebe2f9722
OneEuroQuestion <mike@oneeuroquestion.com>
2025-01-09 11:45:32 +0100

    remove unused code

--- scripts/utils.py
+++
@@ -1,9 +0,0 @@
-import json
-from datetime import datetime
-
-def save_json(data, filename):
-    data["timestamp"] = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    if filename.endswith(".json"):
-        filename = filename[:-5]
-    with open(f"{filename}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json", "w") as f:
-        json.dump(data, f, indent=2)
