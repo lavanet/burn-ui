e310f09765feb53e02a078192f719e2908e547f0
Nadav Levi <nadavlevi@outlook.com>
2025-11-18 12:45:16 +0200

    fix(gitignore): dont ignore consts

---
+++ scripts/consts.py
@@ -0,0 +1,9 @@
+# RPC_URL = "https://internal-rpc-mainnet.lavanet.xyz:443"
+RPC_URL = "https://lava.tendermintrpc.lava.build:443"
+JSINFOBE_API_PROVIDERS_URL = "https://jsinfo.mainnet.lavanet.xyz/providers"
+JSINFOBE_API_ACTIVE_PROVIDERS_URL = "https://jsinfo.mainnet.lavanet.xyz/active_providers"
+JSINFOBE_API_VALIDATORS_URL = "https://jsinfo.mainnet.lavanet.xyz/validators"
+LAVA_REST_URL = "https://lava.rest.lava.build"
+
+
+# lavad q pairing provider-reputation-details "lava@1uhwudw7vzqtnffu2hf5yhv4n8trj79ezl66z99" "*" "*" --node "https://lav1.tendermintrpc.lava.build:443"
